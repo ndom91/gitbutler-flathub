58a8b4f1987f3603e818acefa53e205d75ff2d5b
ndom91 <yo@ndo.dev>
2024-11-28 12:03:41 +0100

    fix: add pnpm and rm compiling webkit2gtk-4.1 since its in gnome47 flatpak SDK

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ MANIFEST_PATH="com.gitbutler.app.yml"
 FLATPAK_ID="com.gitbutler.app"
 
 # Add Flathub Repository
-flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+# flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # Install Gnome runtime dependency
-flatpak install --user flathub org.gnome.Platform//47 org.gnome.Sdk//47
+# flatpak install --user flathub org.gnome.Platform//47 org.gnome.Sdk//47
 
 # Build the manifest
 # flatpak-builder --force-clean --user --install-deps-from=flathub --repo=repo builddir "$MANIFEST_PATH"
@@ -28,3 +28,7 @@ flatpak-builder \
 # - https://github.com/madeofpendletonwool/PinePods/blob/5377b061357d1ba5e04e607b5f626bf349cf09f8/.github/workflows/build-flatpak.yml#L4
 # - https://github.com/Beanow/hi-flatpak
 # - https://github.com/flathub/com.vscodium.codium-insiders/blob/master/com.vscodium.codium-insiders.yaml
+#
+#
+# Instllation
+# - libsoup (2.54+)
